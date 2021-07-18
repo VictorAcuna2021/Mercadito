fdace99af2093c41dab087062621d2d1d807d651
VictorAcuna2021 <Victor.2001@yahoo.com.ar>
2021-07-18 12:40:42 -0300

    modificacion para subir a Heroku

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+// Seteo para el deploy
+app.set("port",process.env.PORT || 3000);
+
 //instanciando el servidor
-app.listen(3000, ()=> console.log("Server on http://localhost:3000"));
+// app.listen(3000, ()=> console.log("Server on http://localhost:3000"));
+// Seteo para el deploy
+app.listen(app.get("port"),()=> console.log("Server Start http://localhost:"+app.get("port")));
 
 //verifico el funcionamiento del servidor
 //app.get("/", (req,res)=> res.send ("Hola Express"));
